95d09dc3a5ed167f19b91335af81df32e9be7412
Mathew <raju332@we36225.pnl.gov>
2019-03-04 13:06:06 -0800

    changing to correct topic for capacitor

--- src/app/topology-renderer/models/ToggleCapacitorRequest.ts
+++ src/app/topology-renderer/models/ToggleCapacitorRequest.ts
@@ -1,8 +1,8 @@
 import { MessageRequest } from '../../models/message-requests/MessageRequest';
 
 export class ToggleCapacitorRequest implements MessageRequest {
-  readonly url = '/topic/goss.gridappsd.fncs.input';
-  readonly replyTo = '/topic/goss.gridappsd.fncs.input.capacitor';
+  readonly url = '/topic/goss.gridappsd.simulation.input';
+  readonly replyTo = '/topic/goss.gridappsd.simulation.input.capacitor';
   constructor(readonly requestBody: any) {
 
   }
