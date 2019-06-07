07372d67446ce7d214939565b2293e7aa123e896
Nhuy Van <nhuy.van@pnnl.gov>
2019-06-07 00:32:07 -0700

    Add a state store service

---
+++ src/app/shared/Store.ts
@@ -0,0 +1,33 @@
+import { Subject } from 'rxjs';
+import { map } from 'rxjs/operators';
+
+export class Store<T> {
+
+  private static readonly _INSTANCE = new Store();
+
+  private readonly _stateChange = new Subject<T>();
+
+  private _state: T;
+
+  static getInstance<T>(): Store<T> {
+    return Store._INSTANCE as Store<T>;
+  }
+
+  initialize(defaultState: T) {
+    if (this._state)
+      throw new Error('Store has been initialized');
+    this._state = defaultState;
+    this._stateChange.next(this._state);
+  }
+
+  mergeState(newState: { [K in keyof T]: T[K] }) {
+    for (const key in newState)
+      this._state[key] = newState[key];
+    this._stateChange.next(this._state);
+  }
+
+  select<K extends keyof T>(selector: (state: T) => T[K]) {
+    return this._stateChange.pipe(map(selector));
+  }
+
+}
