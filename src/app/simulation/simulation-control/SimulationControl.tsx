844a17d8e720bf8a30c0bc8c294ed7e7c7688319
Nhuy Van <nhuy.van@pnnl.gov>
2019-06-13 15:08:28 -0700

    Now clicking on simulation ID will copy it to clipboard

--- src/app/simulation/simulation-control/SimulationControl.tsx
+++ src/app/simulation/simulation-control/SimulationControl.tsx
@@ -3,6 +3,7 @@ import * as React from 'react';
 import { IconButton } from '@shared/buttons';
 import { SimulationStatus } from '@shared/simulation';
 import { Tooltip } from '@shared/tooltip';
+import { Ripple } from '@shared/ripple';
 
 import './SimulationControl.scss';
 
@@ -16,56 +17,109 @@ interface Props {
   onResumeSimulation: () => void;
 }
 
-export const SimulationControl = (props: Props) => (
-  <div className='simulation-control'>
-    {
-      props.simulationId &&
-      <div className='simulation-control__simulation-id'>
-        <span className='simulation-control__simulation-id__label'>Simulation ID</span>
-        <span className='simulation-control__simulation-id__value'>{props.simulationId}</span>
+interface State {
+  simulationIdCopiedSuccessfully: boolean;
+}
+
+export class SimulationControl extends React.Component<Props, State> {
+  constructor(props: Props) {
+    super(props);
+    this.state = {
+      simulationIdCopiedSuccessfully: false
+    };
+
+    this.saveSimulationIdToClipboard = this.saveSimulationIdToClipboard.bind(this);
+  }
+
+  render() {
+    return (
+
+      <div className='simulation-control'>
+        {
+          this.props.simulationId
+          &&
+          <div className='simulation-control__simulation-id'>
+            <span className='simulation-control__simulation-id__label'>Simulation ID</span>
+            <Ripple>
+              <span className='simulation-control__simulation-id__value-wrapper'>
+                {
+                  this.state.simulationIdCopiedSuccessfully
+                  &&
+                  <Tooltip content='Copied to clipboard'>
+                    <span className='simulation-control__simulation-id__value__copied-successfully-tooltip' />
+                  </Tooltip>
+                }
+                <span
+                  className='simulation-control__simulation-id__value'
+                  onClick={this.saveSimulationIdToClipboard}
+                  style={{ pointerEvents: this.state.simulationIdCopiedSuccessfully ? 'none' : 'all' }}>
+                  {this.props.simulationId}
+                </span>
+              </span>
+            </Ripple>
+          </div>
+        }
+        <span className='simulation-control__timestamp'>{this.props.timestamp}</span>
+        {
+          this.props.simulationStatus === SimulationStatus.STARTED || this.props.simulationStatus === SimulationStatus.RESUMED
+            ?
+            <>
+              <Tooltip position='bottom' content='Pause simulation'>
+                <IconButton
+                  icon='pause'
+                  className='simulation-control__action'
+                  onClick={this.props.onPauseSimulation} />
+              </Tooltip>
+              <Tooltip position='bottom' content='Stop simulation'>
+                <IconButton
+                  icon='stop'
+                  className='simulation-control__action'
+                  onClick={this.props.onStopSimulation} />
+              </Tooltip>
+            </>
+            : this.props.simulationStatus === SimulationStatus.PAUSED
+              ?
+              <>
+                <Tooltip position='bottom' content='Resume simulation'>
+                  <IconButton
+                    icon='play_arrow'
+                    className='simulation-control__action resume'
+                    onClick={this.props.onResumeSimulation} />
+                </Tooltip>
+                <Tooltip position='bottom' content='Stop simulation'>
+                  <IconButton
+                    icon='stop'
+                    className='simulation-control__action'
+                    onClick={this.props.onStopSimulation} />
+                </Tooltip>
+              </>
+              :
+              <Tooltip position='bottom' content='Start simulation'>
+                <IconButton
+                  icon='play_arrow'
+                  className='simulation-control__action start'
+                  onClick={this.props.onStartSimulation} />
+              </Tooltip>
+        }
       </div>
-    }
-    <span className='simulation-control__timestamp'>{props.timestamp}</span>
-    {
-      props.simulationStatus === SimulationStatus.STARTED || props.simulationStatus === SimulationStatus.RESUMED
-        ?
-        <>
-          <Tooltip position='bottom' content='Pause simulation'>
-            <IconButton
-              icon='pause'
-              className='simulation-control__action'
-              onClick={props.onPauseSimulation} />
-          </Tooltip>
-          <Tooltip position='bottom' content='Stop simulation'>
-            <IconButton
-              icon='stop'
-              className='simulation-control__action'
-              onClick={props.onStopSimulation} />
-          </Tooltip>
-        </>
-        : props.simulationStatus === SimulationStatus.PAUSED
-          ?
-          <>
-            <Tooltip position='bottom' content='Resume simulation'>
-              <IconButton
-                icon='play_arrow'
-                className='simulation-control__action resume'
-                onClick={props.onResumeSimulation} />
-            </Tooltip>
-            <Tooltip position='bottom' content='Stop simulation'>
-              <IconButton
-                icon='stop'
-                className='simulation-control__action'
-                onClick={props.onStopSimulation} />
-            </Tooltip>
-          </>
-          :
-          <Tooltip position='bottom' content='Start simulation'>
-            <IconButton
-              icon='play_arrow'
-              className='simulation-control__action start'
-              onClick={props.onStartSimulation} />
-          </Tooltip>
-    }
-  </div>
-);
+
+    );
+  }
+
+  saveSimulationIdToClipboard(event: React.SyntheticEvent) {
+    const range = document.createRange();
+    const selection = window.getSelection();
+    range.selectNode(event.target as HTMLElement);
+    selection.addRange(range);
+    this.setState({
+      simulationIdCopiedSuccessfully: document.execCommand('copy')
+    });
+    selection.removeAllRanges();
+    setTimeout(() => {
+      this.setState({
+        simulationIdCopiedSuccessfully: false
+      });
+    }, 2000);
+  }
+
+}
