8129080272b7aa61cd92ddacbe0947924aa6a54d
Nhuy <nhuy@email.com>
2019-09-09 13:47:43 -0700

    Make ripple color configurable for all button types

--- src/app/shared/buttons/basic-button/BasicButton.tsx
+++ src/app/shared/buttons/basic-button/BasicButton.tsx
@@ -5,16 +5,17 @@ import { Ripple } from '../../ripple/Ripple';
 import './BasicButton.scss';
 
 interface Props {
+  label: string;
   type: 'positive' | 'negative';
   className?: string;
-  label: string;
-  onClick: (event: any) => void;
   disabled?: boolean;
+  rippleColor?: string;
+  onClick: (event: React.MouseEvent) => void;
 }
 
 export function BasicButton(props: Props) {
   return (
-    <Ripple>
+    <Ripple color={props.rippleColor || props.type === 'positive' ? 'rgba(250, 250, 250, 0.5)' : undefined}>
       <button
         type='button'
         className={props.type + (props.className ? ` ${props.className}` : '')}
