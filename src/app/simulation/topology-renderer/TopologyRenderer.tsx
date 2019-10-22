51d17484346e40ac2b80d231c00add341cda342f
Nhuy <nhuy@email.com>
2019-10-22 14:47:50 -0700

    We only want to change the color of the toggled switch when we get its new state from the simulation output

--- src/app/simulation/topology-renderer/TopologyRenderer.tsx
+++ src/app/simulation/topology-renderer/TopologyRenderer.tsx
@@ -480,19 +480,13 @@ export class TopologyRenderer extends React.Component<Props, State> {
         onCancel={this.overlay.hide}
         onConfirm={open => {
           this.overlay.hide();
-          this._toggleSwitch(open, swjtch, clickedElement);
+          this._toggleSwitch(open, swjtch);
         }} />
     );
   }
 
-  private _toggleSwitch(open: boolean, swjtch: Switch, clickedElement: Selection<SVGElement, Node, SVGElement, any>) {
+  private _toggleSwitch(open: boolean, swjtch: Switch) {
     if (swjtch.open !== open) {
-      if (clickedElement.classed('topology-renderer__canvas__symbol'))
-        clickedElement.attr('fill', open ? swjtch.colorWhenOpen : swjtch.colorWhenClosed);
-      // The switch circle node was clicked, so update the fill of the switch symbol
-      else
-        select(`.topology-renderer__canvas__symbol.switch._${swjtch.name}_`)
-          .attr('fill', open ? swjtch.colorWhenOpen : swjtch.colorWhenClosed);
       swjtch.open = open;
       this.props.onToggleSwitch(swjtch);
     }
