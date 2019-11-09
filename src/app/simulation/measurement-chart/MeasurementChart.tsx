62c6e5c2168726d7c17b1b3741f0dd68460281d8
Nhuy <nhuy@email.com>
2019-11-09 13:06:07 -0800

    Reduce the number of DOM manipulations when rendering the measurment plots

--- src/app/simulation/measurement-chart/MeasurementChart.tsx
+++ src/app/simulation/measurement-chart/MeasurementChart.tsx
@@ -1,14 +1,13 @@
 import * as React from 'react';
 import { select, Selection } from 'd3-selection';
 import { scaleLinear, scaleTime, ScaleTime, ScaleLinear } from 'd3-scale';
-import { axisBottom, axisLeft } from 'd3-axis';
+import { axisBottom, axisLeft, Axis } from 'd3-axis';
 import { line, Line } from 'd3-shape';
 import { extent } from 'd3-array';
 import { timeFormat } from 'd3-time-format';
 
 import { MeasurementChartModel } from './models/MeasurementChartModel';
 import { TimeSeriesDataPoint } from './models/TimeSeriesDataPoint';
-import { TimeSeries } from './models/TimeSeries';
 
 import './MeasurementChart.light.scss';
 import './MeasurementChart.dark.scss';
@@ -22,24 +21,103 @@ interface State {
 
 export class MeasurementChart extends React.Component<Props, State> {
 
+  readonly width = 370;
   readonly height = 270;
+  readonly margin = {
+    top: 20,
+    bottom: 35,
+    left: 70,
+    right: 10
+  };
+  readonly lineGenerator: Line<TimeSeriesDataPoint>;
 
   canvas: SVGSVGElement = null;
-  width = 370;
 
-  private _container: Selection<SVGGElement, any, any, any> = null;
-  private _timeScale: ScaleTime<number, number> = null;
-  private _yScale: ScaleLinear<number, number> = null;
-  private _lineGenerator: Line<TimeSeriesDataPoint> = null;
-  private readonly _margin = { top: 20, bottom: 35, left: 70, right: 10 };
+  private readonly _xScale: ScaleTime<number, number>;
+  private readonly _yScale: ScaleLinear<number, number>;
+  private readonly _xAxisGenerator: Axis<Date>;
+  private readonly _yAxisGenerator: Axis<number>;
 
-  constructor(props: any) {
+  private _container: Selection<SVGElement, any, SVGElement, any>;
+  private _xAxis: Selection<SVGElement, any, SVGElement, any>;
+  private _yAxis: Selection<SVGGElement, any, SVGElement, any>;
+
+  constructor(props: Props) {
     super(props);
+
+    this._xScale = scaleTime()
+      .range([this.margin.left, this.width - this.margin.right]);
+
+    this._yScale = scaleLinear()
+      .range([this.height - this.margin.bottom, this.margin.top]);
+
+    this.lineGenerator = line<TimeSeriesDataPoint>()
+      .x(dataPoint => this._xScale(dataPoint.unscaledX))
+      .y(dataPoint => this._yScale(dataPoint.unscaledY));
+
+    this._xAxisGenerator = axisBottom<Date>(this._xScale)
+      .tickFormat(timeFormat('%H:%M:%S'));
+
+    this._yAxisGenerator = axisLeft<number>(this._yScale);
   }
 
   componentDidMount() {
-    this._init();
+    this._container = select(this.canvas.querySelector('.measurement-chart__canvas__container') as SVGElement);
+    this._xAxis = this._container.select('.x-axis');
+    this._yAxis = this._container.select('.y-axis');
+
     this._render();
+  }
+
+  private _render() {
+    const axisExtents = this._calculateXYAxisExtents();
+    if (axisExtents.y[0] === axisExtents.y[1])
+      axisExtents.y[0] = 0;
+
+    this._renderXAxis(axisExtents.x);
+    this._renderYAxis(axisExtents.y);
+    this._renderTimeSeriesLineCharts();
+  }
+
+  private _calculateXYAxisExtents(): { x: [Date, Date], y: [number, number] } {
+    const dataPoints: Array<TimeSeriesDataPoint> = this.props.measurementChartModel.timeSeries.reduce(
+      (points, series) => {
+        points.push(...series.points);
+        return points;
+      },
+      []
+    );
+    return {
+      x: extent<TimeSeriesDataPoint, Date>(dataPoints, point => point.unscaledX),
+      y: extent<TimeSeriesDataPoint, number>(dataPoints, point => point.unscaledY)
+    };
+  }
+
+  private _renderXAxis(xAxisExtent: [Date, Date]) {
+    this._xScale.domain(xAxisExtent)
+      .range([this.margin.left, this.width - this.margin.right]);
+    this._xAxisGenerator.scale(this._xScale);
+    this._xAxis.call(this._xAxisGenerator)
+      .selectAll('text')
+      .style('text-anchor', 'end')
+      .attr('dx', '-.4em')
+      .attr('dy', '.45em')
+      .attr('transform', 'rotate(-35)');
+  }
+
+  private _renderYAxis(yAxisExtent: [number, number]) {
+    this._yScale.domain(yAxisExtent);
+    this._yAxisGenerator.scale(this._yScale);
+    this._yAxis.call(this._yAxisGenerator);
+  }
+
+  private _renderTimeSeriesLineCharts() {
+    this._container.selectAll('.measurement-chart__canvas__time-series-line')
+      .data(this.props.measurementChartModel.timeSeries)
+      .join(
+        enter => enter.append('path').attr('class', 'measurement-chart__canvas__time-series-line')
+      )
+      .attr('d', timeSeries => this.lineGenerator(timeSeries.points));
   }
 
   componentDidUpdate(prevProps: Props) {
@@ -74,84 +152,30 @@ export class MeasurementChart extends React.Component<Props, State> {
           height={this.height}
           viewBox={`0 0 ${this.width} ${this.height}`}
           preserveAspectRatio='xMidYMin meet'>
-          <g />
+          <g className='measurement-chart__canvas__container'>
+            <g
+              className='measurement-chart__canvas__axis x-axis'
+              transform={`translate(0,${this.height - this.margin.bottom})`} />
+            <g
+              className='measurement-chart__canvas__axis y-axis'
+              transform={`translate(${this.margin.left},0)`} />
+
+            {
+              this.props.measurementChartModel.yAxisLabel
+              &&
+              <text
+                className='measurement-chart__canvas__axis-label'
+                x={this.margin.left / 2}
+                y={this.margin.top / 2}
+                fontSize='0.8em'
+                fontWeight='bold'>
+                {this.props.measurementChartModel.yAxisLabel}
+              </text>
+            }
+          </g>
         </svg>
       </div>
     );
-  }
-
-  private _init() {
-    this._container = select(this.canvas).select('g');
-    this._timeScale = scaleTime()
-      .range([this._margin.left, this.width - this._margin.right]);
-    this._yScale = scaleLinear()
-      .range([this.height - this._margin.bottom, this._margin.top]);
-    this._lineGenerator = line<TimeSeriesDataPoint>()
-      .x(dataPoint => this._timeScale(dataPoint.unscaledX))
-      .y(dataPoint => this._yScale(dataPoint.unscaledY));
-  }
-
-  private _render() {
-    this._container.selectAll('*').remove();
-
-    const axisExtents = this._calculateXYAxisExtents(this.props.measurementChartModel.timeSeries);
-    if (axisExtents.yExtent[0] === axisExtents.yExtent[1])
-      axisExtents.yExtent[0] = 0;
-    this._timeScale.domain(axisExtents.xExtent);
-    this._yScale.domain(axisExtents.yExtent);
-
-    this._renderYAxisLabel();
-    this._renderXAxis();
-    this._renderYAxis();
-    this._renderTimeSeriesLineCharts();
-  }
-
-  private _calculateXYAxisExtents(timeSeries: TimeSeries[]): { xExtent: [Date, Date], yExtent: [number, number] } {
-    const dataPoints: Array<TimeSeriesDataPoint> = timeSeries.reduce((points, series) => points.concat(series.points), []);
-    return {
-      xExtent: extent<TimeSeriesDataPoint, Date>(dataPoints, point => point.unscaledX),
-      yExtent: extent<TimeSeriesDataPoint, number>(dataPoints, point => point.unscaledY)
-    };
-  }
-
-  private _renderYAxisLabel() {
-    const yAxisLabel = this.props.measurementChartModel.yAxisLabel;
-    if (yAxisLabel)
-      this._container.append('text')
-        .attr('class', 'measurement-chart__canvas__axis-label')
-        .text(yAxisLabel)
-        .attr('x', this._margin.left / 2)
-        .attr('y', this._margin.top / 2)
-        .attr('font-size', '0.8em')
-        .attr('font-weight', 'bold');
-  }
-
-  private _renderXAxis() {
-    this._container.append('g')
-      .attr('class', 'axis x')
-      .attr('transform', `translate(0,${this.height - this._margin.bottom})`)
-      .call(axisBottom(this._timeScale).tickFormat(timeFormat('%H:%M:%S')))
-      .selectAll('text')
-      .style('text-anchor', 'end')
-      .attr('dx', '-.4em')
-      .attr('dy', '.45em')
-      .attr('transform', 'rotate(-35)');
-  }
-
-  private _renderYAxis() {
-    this._container.append('g')
-      .attr('class', 'axis y')
-      .attr('transform', `translate(${this._margin.left},0)`)
-      .call(axisLeft(this._yScale));
-  }
-
-  private _renderTimeSeriesLineCharts() {
-    for (const timeSeries of this.props.measurementChartModel.timeSeries) {
-      this._container.append('path')
-        .attr('class', 'time-series-line')
-        .datum(timeSeries.points)
-        .attr('d', this._lineGenerator);
-    }
   }
 
 }
