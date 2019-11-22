41d7b4bd5942e1c102b45bfc480c537f301eba23
Nhuy <nhuy@email.com>
2019-11-22 14:41:11 -0800

    Show a new indicator during a simulation for the violations at value zero

--- src/app/simulation/voltage-violation/VoltageViolation.tsx
+++ src/app/simulation/voltage-violation/VoltageViolation.tsx
@@ -1,17 +1,58 @@
 import * as React from 'react';
 
+import { Ripple } from '@shared/ripple';
+
 import './VoltageViolation.light.scss';
 import './VoltageViolation.dark.scss';
 
 interface Props {
   violationCounts: number;
+  label: string;
 }
 
-export function VoltageViolation(props: Props) {
-  return (
-    <section className='voltage-violation'>
-      <div className='voltage-violation__label'>Voltage Violation Counts</div>
-      <div className='voltage-violation__counts'>{props.violationCounts}</div>
-    </section>
-  );
+interface State {
+  showLabel: boolean;
+}
+
+export class VoltageViolation extends React.Component<Props, State> {
+
+  constructor(props: Props) {
+    super(props);
+
+    this.state = {
+      showLabel: true
+    };
+
+    this.toggleLabel = this.toggleLabel.bind(this);
+  }
+
+  render() {
+    return (
+      <section className='voltage-violation'>
+        <div
+          className='voltage-violation__label'
+          style={{
+            transform: this.state.showLabel ? 'translateX(0)' : 'translateX(100%)'
+          }}>
+          {this.props.label}
+        </div>
+        <Ripple
+          fixed
+          duration={1500}>
+          <div
+            className='voltage-violation__counts'
+            onClick={this.toggleLabel}>
+            {this.props.violationCounts}
+          </div>
+        </Ripple>
+      </section>
+    );
+  }
+
+  toggleLabel() {
+    this.setState({
+      showLabel: !this.state.showLabel
+    });
+  }
+
 }
