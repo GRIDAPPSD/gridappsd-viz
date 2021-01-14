da580ee190c664e22e7299b67aa182b5b0d7e057
Nhuy <nhuy@email.com>
2021-01-13 20:05:31 -0800

    Switch the arrow directions of selected lines and switches

--- frontend/src/app/shared/topology/Capacitor.ts
+++ frontend/src/app/shared/topology/Capacitor.ts
@@ -8,8 +8,6 @@ export interface Capacitor extends Node {
   kvar_A: number;
   kvar_B: number;
   kvar_C: number;
-  x1: number;
-  y1: number;
   mRID: string;
   controlMode: CapacitorControlMode;
   open: boolean;
